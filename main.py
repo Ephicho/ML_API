a75a88aec479a438455098fec9a99cd1e0c11190
Ephrem Alamerew <58732304+Ephicho@users.noreply.github.com>
2023-10-02 21:05:19 +0300

    APP

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,Form, Body,Path
+from typing import Annotated
+from pydantic import BaseModel, Field
 import joblib
 import pandas as pd
 import numpy as np
 import uvicorn
 from fastapi.responses import JSONResponse
+
 
 app = FastAPI()
 
@@ -18,7 +20,7 @@ scaler = joblib.load(scaler_filepath)
 model = joblib.load(model_filepath)
 
 class PatientData(BaseModel):
-    PRG: float
+    PRG: float 
     PL: float
     PR: float
     SK: float
@@ -28,8 +30,8 @@ class PatientData(BaseModel):
     Age: float
     Insurance: int
 
-def preprocess_input_data(input_data):
-    input_data_df = pd.DataFrame([input_data])
+def preprocess_input_data(user_input):
+    input_data_df = pd.DataFrame([user_input])
     num_columns = [col for col in input_data_df.columns if input_data_df[col].dtype != 'object']
     input_data_imputed_num = num_imputer.transform(input_data_df[num_columns])
     input_scaled_df = pd.DataFrame(scaler.transform(input_data_imputed_num), columns=num_columns)
@@ -37,13 +39,11 @@ def preprocess_input_data(input_data):
 
 @app.get("/")
 def read_root():
-    return "Sepsis Prediction App"
-
+        return "Sepsis Prediction App"
 @app.post("/sepsis/predict")
-def predict_sepsis_endpoint(data: PatientData):
-    input_data = data.dict()
-    input_scaled_df = preprocess_input_data(input_data)
-    
+def get_data_from_user(data:PatientData):
+    user_input = data.dict()
+    input_scaled_df = preprocess_input_data(user_input)
     probabilities = model.predict_proba(input_scaled_df)[0]
     prediction = np.argmax(probabilities)
 
@@ -51,20 +51,43 @@ def predict_sepsis_endpoint(data: PatientData):
     probability = probabilities[1] if prediction == 1 else probabilities[0]
 
     if prediction == 1:
-        status_icon = "✔"
-        sepsis_explanation = "Sepsis is a life-threatening condition. A positive prediction suggests that the patient might be exhibiting sepsis symptoms and requires immediate medical attention."
+        sepsis_explanation = "A positive prediction suggests that the patient might be exhibiting sepsis symptoms and requires immediate medical attention."
     else:
-        status_icon = "✘"
-        sepsis_explanation = "Sepsis is a life-threatening condition. A negative prediction suggests that the patient is not currently exhibiting sepsis symptoms."
+        sepsis_explanation = "A negative prediction suggests that the patient is not currently exhibiting sepsis symptoms."
 
-    statement = f"The patient's sepsis status is {sepsis_status} {status_icon} with a probability of {probability:.2f}. {sepsis_explanation}"
+    statement = f"The patient's sepsis status is {sepsis_status} with a probability of {probability:.2f}. {sepsis_explanation}"
 
-    user_input_statement = "Please note this is the user-inputted data: "
-    output_df = pd.DataFrame([input_data])
+    user_input_statement = "user-inputted data: "
+    output_df = pd.DataFrame([user_input])
 
     result = {'predicted_sepsis': sepsis_status, 'statement': statement, 'user_input_statement': user_input_statement, 'input_data_df': output_df.to_dict('records')}
     return result
-# if __name__ == "__main__":
-#     import uvicorn
 
-#     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+
+
+# @app.post("/sepsis/predict")
+# @app.put("/items{PatientData}")
+# def predict_sepsis_endpoint(data: PatientData):
+#     input_data = data.dict()
+#     input_scaled_df = preprocess_input_data(input_data)
+    
+    # probabilities = model.predict_proba(input_scaled_df)[0]
+    # prediction = np.argmax(probabilities)
+
+    # sepsis_status = "Positive" if prediction == 1 else "Negative"
+    # probability = probabilities[1] if prediction == 1 else probabilities[0]
+
+    # if prediction == 1:
+    #     status_icon = "✔"
+    #     sepsis_explanation = "Sepsis is a life-threatening condition. A positive prediction suggests that the patient might be exhibiting sepsis symptoms and requires immediate medical attention."
+    # else:
+    #     status_icon = "✘"
+    #     sepsis_explanation = "Sepsis is a life-threatening condition. A negative prediction suggests that the patient is not currently exhibiting sepsis symptoms."
+
+    # statement = f"The patient's sepsis status is {sepsis_status} {status_icon} with a probability of {probability:.2f}. {sepsis_explanation}"
+
+    # user_input_statement = "user-inputted data: "
+    # output_df = pd.DataFrame([input_data])
+
+    # result = {'predicted_sepsis': sepsis_status, 'statement': statement, 'user_input_statement': user_input_statement, 'input_data_df': output_df.to_dict('records')}
+    # return result
